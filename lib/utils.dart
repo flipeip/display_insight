6a8ea5ae8bccb21a2d32ea0ab826cafe26c73a65
Felipe Matheus <felipe.mirian.fm@gmail.com>
2024-05-29 11:26:43 -0300

    Correção de tamanho dos itens

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
 
 double sizeConstOf(BuildContext context) {
-  final ratio = MediaQuery.devicePixelRatioOf(context);
-  final size = MediaQuery.sizeOf(context).width / 1980;
-  return size * ratio;
+  final ratio = MediaQuery.sizeOf(context).width / 1980;
+  return ratio;
 }
